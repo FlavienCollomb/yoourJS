eedb65db66c398222f4a07cc494fb079826afd84
Flavien Collomb <social@flaviencollomb.fr>
2015-09-15 22:51:38 +0200

    Update UrTag.js

--- ui/UrTag.js
+++ ui/UrTag.js
@@ -4,11 +4,13 @@
  * @extends UrWidget
  * @author Flavien Collomb
  * @param {Object} settings
- *      @param {String}         [settings.name] UrPopup name
- *      @param {String}         [settings.id] HTML attribute "id" of UrPopup main window widget
- *      @param {String}         [settings.className] HTML attribute "class" of UrPopup main window widget
- *      @param {Object|UrStyle} [settings.style] Style of UrPopup main window widget
- *      @param {UrWidget}       [settings.closeWidget] UrWidget for close UrPopup (for example UrImage with cross).
+ *      @param {String}         [settings.name] UrTag name
+ *      @param {String}         [settings.id] HTML attribute "id" of UrTag main window widget
+ *      @param {String}         [settings.className] HTML attribute "class" of UrTag main window widget
+ *      @param {Object|UrStyle} [settings.style] Style of UrTag main window widget
+ *      @param {String}         [settings.html] HTML content of UrTag
+ *      @param {UrWidget}       [settings.closeWidget] UrWidget for close UrTag (for example UrImage with cross).
+ *      @param {Function}       [settings.next] Function called when close is clicked
  * @example
  *      var body = document.getElementsByTagName("body")[0];
  *      body = new UrWidget({"element": body});
@@ -46,7 +48,7 @@ var UrTag=function(settings){
                 "float":"left"
             };
         UrWidget.call(this, settings, "UrTag");
-        this.setCloseWidget(settings.closeWidget);
+        this.setCloseWidget(settings.closeWidget,settings.next);
     }
 };
 UrTag.prototype=new UrWidget();
@@ -56,12 +58,10 @@ UrTag.prototype.constructor=UrTag;
  * @method setCloseWidget
  * @for UrTag
  * @param {UrWidget} closeWidget
+ * @param {Function} next
  */
-UrTag.prototype.setCloseWidget=function(closeWidget){
+UrTag.prototype.setCloseWidget=function(closeWidget,next){
     var that = this;
-
-    if(this.closeWidgetContainer != undefined)
-        this.closeWidgetContainer.remove();
 
     this.closeWidget = closeWidget;
 
@@ -76,10 +76,16 @@ UrTag.prototype.setCloseWidget=function(closeWidget){
                 "margin":"-5px 2px 0 5px"
             }
         });
+    else
+        this.addChild(closeWidget);
+
     this.addChild(new UrWidget({"parent":this,"style":{"clear":"both"}}));
 
     this.closeWidget.click(function(){
         that.remove();
         that.exist = false;
+
+        if(next!=undefined)
+            next();
     });
 };
